ed01f1cb4bace960b2779d3b9130af29f1a461cf
kwameZion <jessicadonkor@outlook.de>
2022-07-14 12:24:31 +0200

    my second probe projekt

---
+++ greeter.java
@@ -0,0 +1,9 @@
+class Greeter {
+    String name;
+    Greeter (String name) {
+        this.name = name;
+    }
+    public String greet () {
+        return "Hello" + name + " what a wonderful day."
+    }
+}
